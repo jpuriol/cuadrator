b0228b57ad3186cf5c2f91ef6ec1bc3dd453594f
Juan Pablo Uriol Balbín <jpuriol@gmail.com>
2023-02-08 10:32:22 +0100

    Mejorado la salida de stats

--- cmd/root.go
+++ cmd/root.go
@@ -9,16 +9,7 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "cuadrator",
-	Short: "Amplicacion para crear cuadrantes",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Helper to create quadrants, by jpuriol CTEA",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
